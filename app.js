5739ec8e47b7dd4bc517f360ee0955eaa1d0c95f
bipproduction <bip.production.js@gmail.com>
2023-08-26 21:10:34 +0800

    tambahannya

--- app.js
+++ app.js
@@ -3,4 +3,4 @@ const app = express()
 const path = require('path')
 app.use(express.static(path.join(__dirname, "dist")))
 
-app.listen(3003, () => console.log("server berjalan di port 3003"))
+app.listen(3000, () => console.log("server berjalan di port 3000"))
